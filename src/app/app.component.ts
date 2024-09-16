960f482d8b01e80e1ade4ba0f7b5c973b314b66e
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2024-09-15 22:32:01 -0300

    comt inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {JsonViewerComponent} from "./components/json-viewer/json-viewer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, JsonViewerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TCC-FRONT';
+  jsonData = {
+    name: "John Doe",
+    age: 30,
+    city: "New York",
+    profession: "Software Developer",
+    skills: ["Angular", "TypeScript", "JavaScript"],
+    experience: {
+      company: "Tech Corp",
+      years: 5,
+      position: "Senior Developer"
+    }
+  };
+
+  isCollapsed = false;
 }
---
+++ src/app/components/json-viewer/json-viewer.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input} from '@angular/core';
+import {NzCardComponent, NzCardMetaComponent} from "ng-zorro-antd/card";
+import {CommonModule, NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-json-viewer',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NzCardComponent,
+    NzCardMetaComponent,
+    NgForOf
+  ],
+  templateUrl: './json-viewer.component.html',
+  styleUrl: './json-viewer.component.scss'
+})
+export class JsonViewerComponent {
+  @Input() jsonData: any;
+
+  // Função para pegar as chaves do JSON
+  objectKeys = Object.keys;
+
+  // Função para formatar valores
+  formatValue(value: any): string {
+    if (Array.isArray(value)) {
+      return value.join(', ');
+    } else if (typeof value === 'object') {
+      return JSON.stringify(value, null, 2);
+    }
+    return value.toString();
+  }
+}
